4ab2eea2dd67f0eba153e495d51db0bc7705e3f9
William Porto <william.porto@nave.rs>
2021-04-09 18:14:29 -0300

    feat: 🎸 setup app theme and adaptive theme provider

--- app/app-module.tsx
+++ app/app-module.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 
+import { AdaptiveThemeProvider } from 'app/shared/contexts/adaptive-theme';
+
 import { Column } from 'app/shared/components/column';
 import { Text } from 'app/shared/components/text';
 
 export function AppModule() {
   return (
-    <Column
-      flex={1}
-      as={SafeAreaView}
-      p="16px"
-      alignItems="center"
-      justifyContent="center"
-      bg="white">
-      <StatusBar backgroundColor="white" barStyle="dark-content" />
+    <AdaptiveThemeProvider>
+      <Column
+        flex={1}
+        as={SafeAreaView}
+        p="16px"
+        alignItems="center"
+        justifyContent="center"
+        bg="white">
+        <StatusBar backgroundColor="white" barStyle="dark-content" />
 
-      <Text>react-native-discovery-modular-structure</Text>
-    </Column>
+        <Text>react-native-discovery-modular-structure</Text>
+      </Column>
+    </AdaptiveThemeProvider>
   );
 }
---
+++ app/shared/themes/dark.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components';
+
+export const dark: DefaultTheme = {
+  colors: {
+    surface: '#2C2C2E',
+    onSurface: '#F2F2F7',
+    background: '#1C1C1E',
+    onBackground: '#F2F2F7',
+  },
+};
