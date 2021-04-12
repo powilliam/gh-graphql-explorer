489b3beb3a2a40aba346b702a8c27157028afc8c
William Porto <william.porto@nave.rs>
2021-04-12 17:37:09 -0300

    adding light theme

---
+++ app/shared/themes/light.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components';
+
+export const light: DefaultTheme = {
+  colors: {
+    surface: '#E5E5EA',
+    onSurface: '#3A3A3C',
+    background: '#FFFFFF',
+    onBackground: '#1C1C1E',
+  },
+};
